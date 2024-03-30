1b6c8fc223ab7031a1b2c6f02c15d86639816004
SabarinathanE <154896658+SabarinathanE@users.noreply.github.com>
2024-03-30 11:06:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Navbar from './component/Navbar';
 import { Route,Routes } from 'react-router-dom';
-import LibraryProvider from './Context/Library';
 import Books from './Pages/Books';
 import Authors from './Pages/Authers';
 import AddBooks from './Pages/AddBooks';
@@ -13,14 +12,12 @@ function App() {
   return (
     <div>
       <Navbar />
-      <LibraryProvider>
       <Routes>
         <Route path='/' Component={Books}/>
         <Route path='/Authors' Component={Authors}/>
         <Route path='/Addbooks' Component={AddBooks}/>
         <Route path='/AddAuthor' Component={AddAuthor}/>
       </Routes>
-      </LibraryProvider>
     </div>
     
   )
