a02b9d50dd1a049b46286996f0db27367f7854a6
SabarinathanE <154896658+SabarinathanE@users.noreply.github.com>
2024-03-30 11:07:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import LibraryProvider from './Context/Library';
 import App from './App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <LibraryProvider>
   <Router>
     <App/>
-  </Router>,
+  </Router>
+  </LibraryProvider>,
 )
